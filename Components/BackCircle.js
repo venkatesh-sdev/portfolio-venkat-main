ba7db1857a7f565b21b57219108c79f653001fcb
venkatesh-sdev <venkatesh.dev.main@gmail.com>
2022-12-09 11:46:44 +0530

    commit

--- Components/BackCircle.js
+++ Components/BackCircle.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BackCircle = () => {
   return (
     <div className=' justify-center items-center absolute'>
-        <div className='w-[200px] h-[348px] border-2 border-[#ffd89d93] -z-20 animate-ping top-0'></div>
+        <div className='w-[200px] h-[290px] border-2 rounded-full border-[#ffd89d93] -z-20 animate-ping top-0'></div>
     </div>
   )
 }
