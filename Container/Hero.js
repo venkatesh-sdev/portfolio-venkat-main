8e6c3a47ce8d26fcbe6186043b9d47a214c726b6
venkatesh-sdev <venkatesh.dev.main@gmail.com>
2022-12-09 11:53:14 +0530

    commit

--- Container/Hero.js
+++ Container/Hero.js
@@ -71,13 +71,13 @@ const Hero = () => {
         className="flex justify-center items-center  flex-col  mt-10 lg:mt-10"
       >
         <div className=" justify-center items-center absolute">
-          <div className="w-[200px] h-[290px] border-2 rounded-[50%] border-[#ffd89d93] animate-ping top-0"></div>
+          <div className="w-[200px] h-[270px] border-2 rounded-[50%] border-[#ffd89d93] animate-ping top-0"></div>
         </div>
         <div className=" justify-center items-center absolute">
-          <div className="w-[220px] h-[290px] border-2 rounded-[50%] border-[#ffd89d93] animate-ping top-0"></div>
+          <div className="w-[220px] h-[280px] border-2 rounded-[50%] border-[#ffd89d93] animate-ping top-0"></div>
         </div>
         <div className=" justify-center items-center absolute">
-          <div className="w-[240px] h-[290px] border-2 rounded-[50%] border-[#ffd89d93] animate-ping top-0"></div>
+          <div className="w-[240px] h-[280px] border-2 rounded-[50%] border-[#ffd89d93] animate-ping top-0"></div>
         </div>
         <Image
           src={Myimage}
