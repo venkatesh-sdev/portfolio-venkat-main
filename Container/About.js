392028c57b225b27963c1f118fd40e2e34a1491b
venkatesh-sdev <venkatesh.dev.main@gmail.com>
2022-12-09 12:53:35 +0530

    commit

---
+++ Container/About.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+import pngimage from '../Assets/pngwing.com (3).png'
+
+
+const About = () => {
+  return (
+    <div className=''>
+    </div>
+  )
+}
+
+export default About
