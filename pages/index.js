c784c8078954448c602bed4b4dd4e02106fd17a3
venkatesh-sdev <venkatesh.dev.main@gmail.com>
2022-12-09 13:05:00 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import React from "react";
 import { Header, Hero } from "../Container";
+import About from "../Container/About";
 
 const index = () => {
   return (
@@ -9,15 +10,18 @@ const index = () => {
         <title>Venkat-Portfolio</title>
       </Head>
 
-      <header id="#header" className="bg-skin-fill">
+      <header id="header" className="bg-skin-fill">
         <Header />
       </header>
 
       <main>
-        <section id="#hero" className="bg-skin-fill">
+        <section id="hero" className="bg-skin-fill">
           <Hero />
         </section>
       </main>
+      <section id='about' className="h-screen">
+        <About/>
+      </section>
     </React.Fragment>
   );
 };
